aebe521866528f43c7aadedce0757cf391df34bd
unknown <craxwar@gmail.com>
2022-11-16 18:21:45 +0200

    Adding game model

---
+++ project/server/models/gameModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
+
+const gameSchema = new mongoose.Schema({
+    gameName: {
+        type: String,
+        require: true
+    },
+    gameGenre: {
+        type: String,
+        required: true
+    },
+    likes: [{
+        type: ObjectId,
+        ref: 'User'
+    }],
+    ownerId: {
+        type: ObjectId,
+        ref: 'User'
+    },
+}, { timestamps: { createdAt: 'created_at' } });
+
+module.exports = mongoose.model('Game', gameSchema);
