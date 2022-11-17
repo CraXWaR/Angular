286ca06f0ec33baa64a557560a42cded70981154
unknown <craxwar@gmail.com>
2022-11-17 12:05:27 +0200

    Creating server router

--- project/server/router/index.js
+++ project/server/router/index.js
@@ -1,1 +1,6 @@
 //TODO ADD ROUTERS!
+const express = require('express');
+const router = express.Router();
+const { auth } = require('../utils');
+//TODO add like to comments
+module.exports = router
