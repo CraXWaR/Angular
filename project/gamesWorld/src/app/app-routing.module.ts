09db9beeccd01567221cf186049294c0d06a8e15
unknown <craxwar@gmail.com>
2022-11-22 16:03:17 +0200

    Adding home page

--- project/gamesWorld/src/app/app-routing.module.ts
+++ project/gamesWorld/src/app/app-routing.module.ts
@@ -2,19 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
-  { path: 'auth/login', 
-  component: LoginComponent, 
-  data: {
-    title: 'Login Page'
-  }  },
-  { 
-    path: 'auth/register', 
-    component: RegisterComponent, 
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+    data: {
+      title: 'Home Page'
+    }
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+    data: {
+      title: 'Login Page'
+    }
+  },
+  {
+    path: 'auth/register',
+    component: RegisterComponent,
     data: {
       title: 'Register Page'
-    } 
+    }
   },
 ];
 
