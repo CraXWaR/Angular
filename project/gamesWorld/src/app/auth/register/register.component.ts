8b1e17d0ee1e68d53afdb9584dfdf83e49dd1e76
unknown <craxwar@gmail.com>
2022-11-25 15:44:15 +0200

    adding register functionality

--- project/gamesWorld/src/app/auth/register/register.component.ts
+++ project/gamesWorld/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { appEmailDomains } from 'src/app/shared/constants';
+import { appEmailValidator, sameValueGroupValidator } from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
+    password: this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      repeatPassword: []
+    }, {
+      validators: [sameValueGroupValidator('password', 'repeatPassword')]
+    })
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  registerHandler() {
+    console.log(this.form.value);
+    
   }
 
 }
