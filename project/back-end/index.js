0405c18511bdb03305641ec6631a62be2de6ee3c
unknown <craxwar@gmail.com>
2022-11-30 16:22:35 +0200

    Adding server start

--- project/back-end/index.js
+++ project/back-end/index.js
@@ -1,18 +1,18 @@
+require('dotenv').config()
 const express = require('express');
-const app = express();
-const cors = require('./config/cors');
-const server = require('./enviorment');
-const initDatabase = require('./config/db');
-const routes = require('./routes');
-const authMiddleware = require('./middlewares/authMiddle');
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database');
+const routerConfig = require('./config/routes');
 
-app.use(express.json());
-app.use(cors());
-app.use(authMiddleware);
-app.use(routes);
-//Init DB
-initDatabase()
-.then(() => {
-    app.listen(server.PORT, () => console.log(`Server listening on port ${server.PORT}`));
-})
-.catch((error) => console.log(error));
+
+
+start();
+async function start(){
+      
+    const app = express();
+    expressConfig(app);
+    await databaseConfig(app);
+    routerConfig(app);
+
+    app.listen(process.env.PORT, () => console.log('REST Service started!'))
+}
