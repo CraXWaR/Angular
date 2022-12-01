6e2472b2b50d7b08c4c40fbda9778ff80e5b72f9
unknown <craxwar@gmail.com>
2022-12-01 16:50:16 +0200

    Removing not working register

--- project/gamesWorld/src/app/auth/login/login.component.ts
+++ project/gamesWorld/src/app/auth/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,22 +6,8 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- 
-  SERVER_URL = 'http://localhost:3000/login';
 
-  constructor(private http:HttpClient, private activatedRoute: ActivatedRoute, private router: Router) { }
-
-  loginHandler(data: any){
-    this.http.get(this.SERVER_URL, data)
-      .subscribe((result) => {
-        console.warn('result', result);
-        
-      })
-
-    // const returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
-    
-    // this.router.navigate([returnUrl]);
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
--- project/gamesWorld/src/app/auth/register/register.component.ts
+++ project/gamesWorld/src/app/auth/register/register.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { UserService } from '../services/auth.service';
-
 
 @Component({
   selector: 'app-register',
@@ -10,30 +7,9 @@ import { UserService } from '../services/auth.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private _auth: UserService, private formBuilder: FormBuilder) { }
-
-  signUpForm!: FormGroup;
-  errorMessage = '';
-  isLoading: boolean = false;
+  constructor() { }
 
   ngOnInit(): void {
-    this.signUpForm = new FormGroup ({
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      username: new FormControl(null, [Validators.required, Validators.minLength(5)]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
-      rePassword: new FormControl(null,  [Validators.required, Validators.minLength(5)]),
-    })
-  }
-
-  onRegister(){
-    console.log(this.signUpForm.controls)
-    const {username, email, password, rePassword} = this.signUpForm.value;
-    
-    if(password !== rePassword){
-      this.errorMessage = 'Passwords Don\'t Match';
-      return;
-    }
-    this._auth.registerUser(this.signUpForm.value)
   }
 
 }
