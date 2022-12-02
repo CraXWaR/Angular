8ae46e82aab9ef7416c854bae890395d14646444
unknown <craxwar@gmail.com>
2022-12-02 15:56:51 +0200

    Creating register post request

---
+++ project/server/controllers/authController.js
@@ -0,0 +1,14 @@
+const authController = require('express').Router();
+const { login, register } = require('../service/userService');
+
+authController.post('/register', async (req, res) => {
+    try {
+        const token = await register(req.body.username, req.body.email, req.body.password);
+        res.status(201).json(token);
+        res.end();
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+});
+
+module.exports = authController;
