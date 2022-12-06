a734af9010f7f94e3a40dae8ef8afafc0826cf61
unknown <craxwar@gmail.com>
2022-12-06 16:01:07 +0200

    Changing database name

--- project/server/configs/database.js
+++ project/server/configs/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function initDatabase(){
-    return mongoose.connect('mongodb://0.0.0.0:27017/test');
+    return mongoose.connect('mongodb://0.0.0.0:27017/gamesWorld');
 }
 
 module.exports = initDatabase;
